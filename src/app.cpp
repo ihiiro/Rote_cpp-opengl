6f1daa843c10511432f559b4b6e4f25bacbd56a0
hiro <yassirelyakine@gmail.com>
2022-10-02 18:16:49 +0100

    some nice looking stuff

--- src/app.cpp
+++ src/app.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
+#include <cmath>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 // the code is in GLSL (OpenGL Shader Language)
 const char* VERTEX_SHADER_SOURCE = "#version 330 core\n"
                                    "layout (location = 0) in vec3 apos;\n"
+                                   "layout (location = 1) in vec3 a_color;\n"
+                                   "out vec3 color_set_from_vertices_array;\n"
                                    "void main() {\n"
-                                   "  gl_Position = vec4(apos.x, apos.y, apos.z, 1.0);\n"
+                                   "  gl_Position = vec4(apos, 1.0);\n"
+                                   "  color_set_from_vertices_array = a_color;\n"
                                    "}\0";
 const char* FRAGMENT_SHADER_SOURCE = "#version 330 core\n"
+                                     "in vec3 color_set_from_vertices_array;\n"
                                      "out vec4 frag_color;\n"
                                      "void main() {\n"
-                                     "  frag_color = vec4(1.0f, .5f, .2f, 1.0f);\n"
+                                     "  frag_color = vec4(color_set_from_vertices_array, 1.0f);\n"
                                      "}\0";
-const char* FRAGMENT_SHADER_SOURCE_YELLOW = "#version 330 core\n"
+const char* FRAGMENT_SHADER_SOURCE_GREEN = "#version 330 core\n"
                                             "out vec4 frag_color;\n"
+                                            "uniform vec4 color_from_opengl_code;\n"
                                             "void main() {\n"
-                                            "  frag_color = vec4(1.0f, 1.0f, .0f, 1.0f);\n"
+                                            "  frag_color = color_from_opengl_code;\n"
                                             "}\0";
 
 // non-source constants
@@ -71,7 +77,7 @@ int main() {
   }
 
   unsigned int fragment_shader_yellow = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragment_shader_yellow, 1, &FRAGMENT_SHADER_SOURCE_YELLOW, NULL);
+  glShaderSource(fragment_shader_yellow, 1, &FRAGMENT_SHADER_SOURCE_GREEN, NULL);
   glCompileShader(fragment_shader_yellow);
   glGetShaderiv(fragment_shader_yellow, GL_COMPILE_STATUS, &success);
   if (!success) {
@@ -89,13 +95,13 @@ int main() {
     std::cout << "\nERROR::SHADER_PROGRAM::LINKING_FAILED\n" << info_log << std::endl;
   }
 
-  unsigned int shader_program_yellow = glCreateProgram();
-  glAttachShader(shader_program_yellow, vertex_shader);
-  glAttachShader(shader_program_yellow, fragment_shader_yellow);
-  glLinkProgram(shader_program_yellow);
-  glGetProgramiv(shader_program_yellow, GL_LINK_STATUS, &success);
+  unsigned int shader_program_green = glCreateProgram();
+  glAttachShader(shader_program_green, vertex_shader);
+  glAttachShader(shader_program_green, fragment_shader_yellow);
+  glLinkProgram(shader_program_green);
+  glGetProgramiv(shader_program_green, GL_LINK_STATUS, &success);
   if (!success) {
-    glGetProgramInfoLog(shader_program_yellow, INFO_LOG_SIZE, NULL, info_log);
+    glGetProgramInfoLog(shader_program_green, INFO_LOG_SIZE, NULL, info_log);
     std::cout << "\nERROR::SHADER_PROGRAM_YELLOW::LINKING_FAILED\n" << info_log << std::endl;
   }
 
@@ -103,17 +109,23 @@ int main() {
   glDeleteShader(fragment_shader);
   glDeleteShader(fragment_shader_yellow);
 
+  // show maximum number of vertex attributes supported
+  int nr_attributes;
+  glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nr_attributes);
+  std::cout << "Maximum nr of vertex attributes supported: " << nr_attributes << std::endl;
 
-  float vertices[][9] = {
+
+
+  float vertices[][18] = {
+    {
+      1.0f, -.5f, .0f,  1.0f, .0f, .0f,
+      .1f, -.5f, .0f,   .0f, 1.0f, .0f,
+      .55f, .5f, .0f,   .0f, .0f, 1.0f,
+    },
     {
       -1.0f, -.5f, .0f,
       -.1f, -.5f, .0f,
       -.55f, .5f, .0f,
-    },
-    {
-      1.0f, -.5f, .0f,
-      .1f, -.5f, .0f,
-      .55f, .5f, .0f,
     },
   };
   unsigned int vertex_array_objects[2], vertex_buffer_objects[2];
@@ -126,8 +138,11 @@ int main() {
   glBindVertexArray(vertex_array_objects[0]);
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_objects[0]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0]), vertices[0], GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+  // for color vertex attributes
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   glBindVertexArray(vertex_array_objects[1]);
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_objects[1]);
@@ -150,7 +165,12 @@ int main() {
     glBindVertexArray(vertex_array_objects[0]);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    glUseProgram(shader_program_yellow);
+    glUseProgram(shader_program_green);
+    float time_value = glfwGetTime();
+    float green_value = (sin(time_value) / 2.0f) + .5f;
+    int vertex_color_location = glGetUniformLocation(shader_program_green, "color_from_opengl_code");
+    glUniform4f(vertex_color_location, .0f, green_value, .0f, 1.0f);
+
     glBindVertexArray(vertex_array_objects[1]);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -162,7 +182,7 @@ int main() {
   glDeleteVertexArrays(2, vertex_array_objects);
   glDeleteBuffers(2, vertex_buffer_objects);
   glDeleteProgram(shader_program);
-  glDeleteProgram(shader_program_yellow);
+  glDeleteProgram(shader_program_green);
 
   glfwTerminate();
   return 0;
