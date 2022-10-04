624872f7495dc068b402d45541e3cdc59fed963d
hiro <yassirelyakine@gmail.com>
2022-10-04 22:22:28 +0100

    exercise passed: use uniform to horizontally offset the triangle's position to the right side of the screen via vertex shader

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <shader.h>
@@ -34,7 +33,6 @@ int main() {
   }
 
   Shader shader("/home/hiro/rote/cpp-opengl/project/src/shader.vs", "/home/hiro/rote/cpp-opengl/project/src/shader.fs");
-  Shader shader_green("/home/hiro/rote/cpp-opengl/project/src/shader.vs", "/home/hiro/rote/cpp-opengl/project/src/shader_green.fs");
 
   // show maximum number of vertex attributes supported
   int nr_attributes ;
@@ -43,37 +41,26 @@ int main() {
 
   float vertices[][18] {
     {
-      1.0f, -.5f, .0f,  1.0f, .0f, .0f,
-      .1f, -.5f, .0f,   .0f, 1.0f, .0f,
-      .55f, .5f, .0f,   .0f, .0f, 1.0f,
-    },
-    {
-      -1.0f, -.5f, .0f,
-      -.1f, -.5f, .0f,
-      -.55f, .5f, .0f,
+      .0f, .5f, .0f,  1.0f, .0f, .0f,
+      -.5f, -.5f, .0f,   .0f, 1.0f, .0f,
+      .5f, -.5f, .0f,   .0f, .0f, 1.0f,
     },
   };
-  unsigned int vertex_array_objects[2], vertex_buffer_objects[2];
+  unsigned int vertex_array_object, vertex_buffer_object;
 
   // generate objects
-  glGenVertexArrays(2, vertex_array_objects);
-  glGenBuffers(2, vertex_buffer_objects);
+  glGenVertexArrays(1, &  vertex_array_object);
+  glGenBuffers(1, &vertex_buffer_object);
 
   // bind/configure objects
-  glBindVertexArray(vertex_array_objects[0]);
-  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_objects[0]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0]), vertices[0], GL_STATIC_DRAW);
+  glBindVertexArray(vertex_array_object);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
   // for color vertex attributes
   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
-
-  glBindVertexArray(vertex_array_objects[1]);
-  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_objects[1]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[1]), vertices[1], GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);
 
   /* glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); does not fill the triangles */
 
@@ -87,16 +74,8 @@ int main() {
 
     // rendering
     shader.use();
-    glBindVertexArray(vertex_array_objects[0]);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-    shader_green.use();
-    float time_value = glfwGetTime();
-    float green_value = (sin(time_value) / 2.0f) + .5f;
-    float rgba_value[] {.0f, green_value, .0f, 1.0f};
-    shader_green.set_float_sin("color_from_opengl_code", rgba_value);
-
-    glBindVertexArray(vertex_array_objects[1]);
+    shader.set_float("horizontal_offset_value", .5f);
+    glBindVertexArray(vertex_array_object);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     // check and call events and swap the buffers
@@ -104,8 +83,8 @@ int main() {
     glfwPollEvents();
   }
 
-  glDeleteVertexArrays(2, vertex_array_objects);
-  glDeleteBuffers(2, vertex_buffer_objects);
+  glDeleteVertexArrays(1, &vertex_array_object);
+  glDeleteBuffers(1, &vertex_buffer_object);
 
   glfwTerminate();
   return 0;
--- src/shader.vs
+++ src/shader.vs
@@ -2,7 +2,10 @@
 layout (location = 0) in vec3 apos;
 layout (location = 1) in vec3 a_color;
 out vec3 color_set_from_vertices_array;
+
+uniform float horizontal_offset_value;
+
 void main() {
-  gl_Position = vec4(apos.x, -apos.y, apos.z, 1.0);
+  gl_Position = vec4(apos.x + horizontal_offset_value, apos.yz, 1.0);
   color_set_from_vertices_array = a_color;
 }
